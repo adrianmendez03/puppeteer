e4ba7e3c4cc7c8e745e282a07dcdbf6959e7bd3d
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-13 18:58:00 -0500

    Login works now

--- scrappers.js
+++ scrappers.js
@@ -1,11 +1,23 @@
 const puppeteer = require('puppeteer');
+const { login } = require('./login.js');
 
 (async () => {
+    async function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.youtube.com', {
+
+    await page.goto('https://www.binarysearch.com', {
         waitUntil: 'networkidle0'
     })
+
+    await login(page)
+
+    await sleep(10000)
+
     await page.screenshot({ path: 'me.png' })
+
     await browser.close()
 })();
