1e6ddea560aee0d7633d7f9687ae43a24825ae76
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-31 21:34:37 -0500

    Updated Sleep

--- scrappers.js
+++ scrappers.js
@@ -1,23 +1,23 @@
-const puppeteer = require('puppeteer');
-const { login } = require('./login.js');
+const puppeteer = require("puppeteer")
+const { login } = require("./login.js")
 
-(async () => {
-    async function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
+;(async () => {
+  async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-    await page.goto('https://www.binarysearch.com', {
-        waitUntil: 'networkidle0'
-    })
+  await page.goto("https://www.binarysearch.com", {
+    waitUntil: "networkidle0",
+  })
 
-    await login(page)
+  await login(page)
 
-    await sleep(10000)
+  await sleep(5000)
 
-    await page.screenshot({ path: 'me.png' })
+  await page.screenshot({ path: "me.png" })
 
-    await browser.close()
-})();
+  await browser.close()
+})()
